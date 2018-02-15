6f88a8bd84f7be1c112fef14d8e10d7d35dfd723
connorbarkr <connorbarkr@gmail.com>
2018-02-15 12:16:28 -0500

    Added animations & jQuery

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,32 @@
 var x = window.matchMedia("(min-width: 768px)");
 
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      setTimeout(() => {
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      }, 500);
+    } // End if
+  });
+});
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
