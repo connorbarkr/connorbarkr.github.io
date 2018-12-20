c33503645257bf84936b7c723f33d9135f21fcfa
connorbarkr <connorbarkr@gmail.com>
2018-12-20 10:47:45 -0500

    Contact loading fix, IE scroll fix

--- src/utils/scrollTo.js
+++ src/utils/scrollTo.js
@@ -1,16 +1,20 @@
 import { isMobile } from './detectMobile.js';
 
-let isSamsungBrowser = navigator.userAgent.match(/SamsungBrowser/i);
+let isNoScrollBrowser = !!(navigator.userAgent.match(/SamsungBrowser/i) ||
+                          navigator.userAgent.match(/Trident/i) ||
+                          navigator.userAgent.match(/rv:11/i) ||
+                          navigator.userAgent.match(/MSIE/i) ||
+                          navigator.userAgent.match(/Edge/i));
 
 export const scrollTo = (ref, behavior, bottom) => {
   let offset = isMobile('lg') ? 0 : 45;
 
   if (bottom) {
-    isSamsungBrowser ? window.scrollTo(0, document.body.scrollHeight) :
+    isNoScrollBrowser ? window.scrollTo(0, document.body.scrollHeight) :
     window.scrollTo({top: document.body.scrollHeight, behavior: behavior ? behavior : 'auto'});
     return;
   }
-  isSamsungBrowser ? window.scrollTo(0, ref.current.offsetTop - offset) :
+  isNoScrollBrowser ? window.scrollTo(0, ref.current.offsetTop - offset) :
   window.scrollTo({
     top: ref.current.offsetTop - offset,
     behavior: behavior ? behavior : 'auto',
